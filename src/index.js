2d7b959e4b4319f5509137400b70e34b33a92d53
ulmasbek <[rakhmatullaevulmas@gmail.com]>
2020-03-10 16:06:49 +0500

    feature: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let checkStr = (searchClosed) => {
+        let currentChar = str[0];
+        str = str.slice(1);
+        if (searchClosed !== undefined && currentChar === searchClosed) {
+            return true;
+        } else {
+            let currentBrackets = bracketsConfig.find(x => x[0] === currentChar);
+            if (currentBrackets === undefined) return false;
+            let ret = (checkStr(currentBrackets[1]) && (searchClosed === undefined || checkStr(searchClosed)));
+            return ret;
+        }
+    };
+    let retValue = true;
+    while (str && str.length > 0 && retValue) {
+        retValue = (retValue && checkStr());
+    }
+    return retValue;
 }
